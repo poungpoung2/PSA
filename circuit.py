4fdbbc38f69b4847c000b3e0c33e1a30e1abcd9c
poungpoung2 <ppharryktk1234@gmail.com>
2025-01-22 11:43:02 -0500

    final version

--- circuit.py
+++ circuit.py
@@ -16,31 +16,63 @@ isWhite = False
 # Function to turn on LEDs
 def turnOn():
     if isWhite:
-        g_led.value = True  
-    r_led.value = True
-    b_led.value = True
+        g_led_1.value = True  
+        g_led_2.value = True  
+        g_led_3.value = True  
+
+    r_led_1.value = True  
+    r_led_2.value = True  
+    r_led_3.value = True  
+
+    b_led_1.value = True  
+    b_led_2.value = True  
+    b_led_3.value = True  
+
 
 # Function to turn off LEDs
 def turnOff():
-    r_led.value = False
-    g_led.value = False
-    b_led.value = False
+    r_led_1.value = False  
+    r_led_2.value = False  
+    r_led_3.value = False  
+
+    g_led_1.value = False  
+    g_led_2.value = False  
+    g_led_3.value = False  
+
+    b_led_1.value = False  
+    b_led_2.value = False  
+    b_led_3.value = False  
 
 # Configure the GPIO pins for LEDs as digital outputs
-r_led = digitalio.DigitalInOut()  
-g_led = digitalio.DigitalInOut()
-b_led = digitalio.DigitalInOut()
+r_led_1 = digitalio.DigitalInOut(board.GP22)  
+r_led_1.direction = digitalio.Direction.OUTPUT
+r_led_2 = digitalio.DigitalInOut(board.GP26)  
+r_led_2.direction = digitalio.Direction.OUTPUT
+r_led_3 = digitalio.DigitalInOut(board.GP27)  
+r_led_3.direction = digitalio.Direction.OUTPUT
 
-r_led.direction = digitalio.Direction.OUTPUT
-g_led.direction = digitalio.Direction.OUTPUT
-b_led.direction = digitalio.Direction.OUTPUT
+
+g_led_1 = digitalio.DigitalInOut(board.GP21)  
+g_led_1.direction = digitalio.Direction.OUTPUT
+g_led_2 = digitalio.DigitalInOut(board.GP20)  
+g_led_2.direction = digitalio.Direction.OUTPUT
+g_led_3 = digitalio.DigitalInOut(board.GP19)  
+g_led_3.direction = digitalio.Direction.OUTPUT
+
+
+b_led_1 = digitalio.DigitalInOut(board.GP18)  
+b_led_1.direction = digitalio.Direction.OUTPUT
+b_led_2 = digitalio.DigitalInOut(board.GP17)  
+b_led_2.direction = digitalio.Direction.OUTPUT
+b_led_3 = digitalio.DigitalInOut(board.GP16)  
+b_led_3.direction = digitalio.Direction.OUTPUT
 
 # Configure the GPIO pin connected to the button as a digital input with pull-up resistor
-onoff_button = digitalio.DigitalInOut()  # Set actual pin
+onoff_button = digitalio.DigitalInOut(board.GP13)  # Set actual pin
 onoff_button.direction = digitalio.Direction.INPUT
 onoff_button.pull = digitalio.Pull.UP
 
-white_button = digitalio.DigitalInOut()  # Set actual pin
+white_button = digitalio.DigitalInOut(board.GP11)  # Set actual pin
 white_button.direction = digitalio.Direction.INPUT
 white_button.pull = digitalio.Pull.UP
 
